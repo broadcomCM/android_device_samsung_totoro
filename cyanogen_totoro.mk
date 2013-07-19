519e694dbed54643474ff7372df00f696d8ed2ce
bieltv3 <bieltv.3@gmail.com>
2013-07-19 16:48:21 +0200

    LDPI boot animation

--- cyanogen_totoro.mk
+++ cyanogen_totoro.mk
@@ -21,11 +21,11 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S5360 BUILD_ID=GRI40 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=samsung/GT-S5360/GT-S5360:2.3.6/GINGERBREAD/XWKTN:user/release-keys PRIVATE_BUILD_DESC="GT-S5360-user 2.3.6 GINGERBREAD XWKTN release-keys"
 
 # Extra overlay for MDPI
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/mdpi
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
 
 # Copy bootanimation
 PRODUCT_COPY_FILES += \
-     vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
+     vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyY
@@ -36,4 +36,4 @@ PRODUCT_VERSION_DEVICE_SPECIFIC :=
 # Copy legend specific prebuilt files
 #
 PRODUCT_COPY_FILES +=  \
-    vendor/cyanogen/prebuilt/mdpi/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
