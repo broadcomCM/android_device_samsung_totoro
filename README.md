3b7b0c07f54034c642c363bcd79bcb87ef7c3460
bieltv3 <bieltv.3@gmail.com>
2013-08-02 01:01:59 +0200

    First push AOSP

---
+++ README.md
@@ -0,0 +1,12 @@
+Device Configuration AOSP GB For Galaxy Y
+=========================================
+
+Device tree for Samsung Galaxy Y S5360 - AOSP-GB
+
+Maintainer: Percy-g2 & PsychoGame & Spacecaker(aosp-gb)
+
+When compiling for this galaxy y 
+do this 
+copy paste spacecaker.patch to $root/frameworks/base
+then cd to $root/frameworks/base and do 
+patch -p1 < spacecaker.patch
---
+++ full_totoro.mk
@@ -0,0 +1,73 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit products
+$(call inherit-product, device/samsung/bcm21553-common/common.mk)
+$(call inherit-product, vendor/samsung/totoro/vendor_blobs.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Using MMB vendor (Superuser, hosts file and APNS)
+$(call inherit-product, vendor/mmb/vendor_blobs.mk)
+
+# Including GApps
+$(call inherit-product, vendor/google/tiny.mk)
+
+# This is where we'd set a backup provider if we had one
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+PRODUCT_NAME := totoro
+PRODUCT_BRAND := samsung
+PRODUCT_DEVICE := totoro
+PRODUCT_MODEL := GT-S5360
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-S5360 BUILD_ID=totoro-userdebug eng.spacecaker.01.08.13 BUILD_DISPLAY_ID=totoro-userdebug eng.spacecaker.01.08.13 BUILD_FINGERPRINT=samsung/GT-S5360/GT-S5360:2.3.6/GINGERBREAD/XWKTN:user/release-keys PRIVATE_BUILD_DESC="GT-S5360-user 2.3.6 GINGERBREAD XWKTN release-keys"
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := GalaxyY
+
+# Add device package overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/totoro/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/mdpi
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+# Add LDPI assets, in addition to MDPI
+PRODUCT_LOCALES += ldpi mdpi
+
+# Kernel modules
+PRODUCT_COPY_FILES += \
+    device/samsung/totoro/prebuilt/root/fsr.ko:root/fsr.ko \
+    device/samsung/totoro/prebuilt/root/fsr_stl.ko:root/fsr_stl.ko \
+    device/samsung/totoro/prebuilt/root/rfs_fat.ko:root/rfs_fat.ko \
+    device/samsung/totoro/prebuilt/root/rfs_glue.ko:root/rfs_glue.ko \
+    device/samsung/totoro/prebuilt/root/j4fs.ko:root/j4fs.ko \
+    device/samsung/totoro/prebuilt/root/sec_param.ko:root/sec_param.ko
+
+# Board-specific init
+PRODUCT_COPY_FILES += \
+    device/samsung/totoro/ramdisk/init.rc:root/init.rc \
+    device/samsung/totoro/ramdisk/init.bcm21553.rc:root/init.bcm21553.rc
+
+# Prebuilt Kernel - DELETE from the package
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/samsung/totoro/prebuilt/kernel
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
